6f6106f91b5b8ac64a37e80d6c4b0bffe84115fd
mehedihasan-ofc <mehedi.ofcc@gmail.com>
2024-11-20 17:36:37 +0600

    Added Orders dashboard 1

--- src/pages/Dashboard/Orders/Orders.jsx
+++ src/pages/Dashboard/Orders/Orders.jsx
@@ -16,9 +16,17 @@ import {
     IconButton,
     Tooltip,
 } from "@material-tailwind/react";
-import { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { useContext, useState } from "react";
+import { AuthContext } from "../../../provider/AuthProvider";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import MySpinner from "../../../components/Shared/MySpinner/MySpinner";
 
 const TABS = [
+    {
+        label: "All",
+        value: "all",
+    },
     {
         label: "Pending",
         value: "pending",
@@ -100,8 +108,22 @@ const TABLE_ROWS = [
 ];
 
 const Orders = () => {
+    const [activeTab, setActiveTab] = useState("all");
 
-    const [activeTab, setActiveTab] = useState("pending");
+    const { user } = useContext(AuthContext);
+    const token = localStorage.getItem('access-token');
+    const [axiosSecure] = useAxiosSecure();
+
+    const { data: orders = [], isLoading, refetch } = useQuery({
+        queryKey: ['orders', user?.email, activeTab],
+        enabled: !!user?.email && !!token,
+        queryFn: async () => {
+            const res = await axiosSecure(`https://new-havit-shop-server.vercel.app/orders/${activeTab}`);
+            return res.data;
+        },
+    });
+
+    console.log(orders);
 
     return (
         <Card className="h-full w-full">
@@ -115,7 +137,7 @@ const Orders = () => {
                                     "rounded-none",
                             }}>
                             {TABS.map(({ label, value }) => (
-                                <Tab onClick={() => setActiveTab(value)} key={value} value={value} className="text-sm">
+                                <Tab onClick={() => setActiveTab(value)} key={value} value={value} className="text-xs">
                                     &nbsp;&nbsp;{label}&nbsp;&nbsp;
                                 </Tab>
                             ))}
@@ -130,108 +152,111 @@ const Orders = () => {
                     </div>
                 </div>
             </CardHeader>
-            <CardBody className="overflow-scroll px-0">
-                <table className="mt-4 w-full min-w-max table-auto text-left">
-                    <thead>
-                        <tr>
-                            {TABLE_HEAD.map((head) => (
-                                <th
-                                    key={head}
-                                    className="border-y border-blue-gray-100 bg-blue-gray-50/50 p-4"
-                                >
-                                    <Typography
-                                        variant="small"
-                                        color="blue-gray"
-                                        className="font-normal leading-none opacity-70"
-                                    >
-                                        {head}
-                                    </Typography>
-                                </th>
-                            ))}
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {TABLE_ROWS.map(
-                            ({ img, name, email, job, org, online, date }, index) => {
-                                const isLast = index === TABLE_ROWS.length - 1;
-                                const classes = isLast
-                                    ? "p-4"
-                                    : "p-4 border-b border-blue-gray-50";
+            {
+                isLoading ? <MySpinner /> :
+                    <CardBody className="overflow-scroll px-0">
+                        <table className="mt-4 w-full min-w-max table-auto text-left">
+                            <thead>
+                                <tr>
+                                    {TABLE_HEAD.map((head) => (
+                                        <th
+                                            key={head}
+                                            className="border-y border-blue-gray-100 bg-blue-gray-50/50 p-4"
+                                        >
+                                            <Typography
+                                                variant="small"
+                                                color="blue-gray"
+                                                className="font-normal leading-none opacity-70"
+                                            >
+                                                {head}
+                                            </Typography>
+                                        </th>
+                                    ))}
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {TABLE_ROWS.map(
+                                    ({ img, name, email, job, org, online, date }, index) => {
+                                        const isLast = index === TABLE_ROWS.length - 1;
+                                        const classes = isLast
+                                            ? "p-4"
+                                            : "p-4 border-b border-blue-gray-50";
 
-                                return (
-                                    <tr key={name}>
-                                        <td className={classes}>
-                                            <div className="flex items-center gap-3">
-                                                <Avatar src={img} alt={name} size="sm" />
-                                                <div className="flex flex-col">
+                                        return (
+                                            <tr key={name}>
+                                                <td className={classes}>
+                                                    <div className="flex items-center gap-3">
+                                                        <Avatar src={img} alt={name} size="sm" />
+                                                        <div className="flex flex-col">
+                                                            <Typography
+                                                                variant="small"
+                                                                color="blue-gray"
+                                                                className="font-normal"
+                                                            >
+                                                                {name}
+                                                            </Typography>
+                                                            <Typography
+                                                                variant="small"
+                                                                color="blue-gray"
+                                                                className="font-normal opacity-70"
+                                                            >
+                                                                {email}
+                                                            </Typography>
+                                                        </div>
+                                                    </div>
+                                                </td>
+                                                <td className={classes}>
+                                                    <div className="flex flex-col">
+                                                        <Typography
+                                                            variant="small"
+                                                            color="blue-gray"
+                                                            className="font-normal"
+                                                        >
+                                                            {job}
+                                                        </Typography>
+                                                        <Typography
+                                                            variant="small"
+                                                            color="blue-gray"
+                                                            className="font-normal opacity-70"
+                                                        >
+                                                            {org}
+                                                        </Typography>
+                                                    </div>
+                                                </td>
+                                                <td className={classes}>
+                                                    <div className="w-max">
+                                                        <Chip
+                                                            variant="ghost"
+                                                            size="sm"
+                                                            value={online ? "online" : "offline"}
+                                                            color={online ? "green" : "blue-gray"}
+                                                        />
+                                                    </div>
+                                                </td>
+                                                <td className={classes}>
                                                     <Typography
                                                         variant="small"
                                                         color="blue-gray"
                                                         className="font-normal"
                                                     >
-                                                        {name}
+                                                        {date}
                                                     </Typography>
-                                                    <Typography
-                                                        variant="small"
-                                                        color="blue-gray"
-                                                        className="font-normal opacity-70"
-                                                    >
-                                                        {email}
-                                                    </Typography>
-                                                </div>
-                                            </div>
-                                        </td>
-                                        <td className={classes}>
-                                            <div className="flex flex-col">
-                                                <Typography
-                                                    variant="small"
-                                                    color="blue-gray"
-                                                    className="font-normal"
-                                                >
-                                                    {job}
-                                                </Typography>
-                                                <Typography
-                                                    variant="small"
-                                                    color="blue-gray"
-                                                    className="font-normal opacity-70"
-                                                >
-                                                    {org}
-                                                </Typography>
-                                            </div>
-                                        </td>
-                                        <td className={classes}>
-                                            <div className="w-max">
-                                                <Chip
-                                                    variant="ghost"
-                                                    size="sm"
-                                                    value={online ? "online" : "offline"}
-                                                    color={online ? "green" : "blue-gray"}
-                                                />
-                                            </div>
-                                        </td>
-                                        <td className={classes}>
-                                            <Typography
-                                                variant="small"
-                                                color="blue-gray"
-                                                className="font-normal"
-                                            >
-                                                {date}
-                                            </Typography>
-                                        </td>
-                                        <td className={classes}>
-                                            <Tooltip content="Edit User">
-                                                <IconButton variant="text">
-                                                    <PencilIcon className="h-4 w-4" />
-                                                </IconButton>
-                                            </Tooltip>
-                                        </td>
-                                    </tr>
-                                );
-                            },
-                        )}
-                    </tbody>
-                </table>
-            </CardBody>
+                                                </td>
+                                                <td className={classes}>
+                                                    <Tooltip content="Edit User">
+                                                        <IconButton variant="text">
+                                                            <PencilIcon className="h-4 w-4" />
+                                                        </IconButton>
+                                                    </Tooltip>
+                                                </td>
+                                            </tr>
+                                        );
+                                    },
+                                )}
+                            </tbody>
+                        </table>
+                    </CardBody>
+            }
         </Card>
     );
 };
