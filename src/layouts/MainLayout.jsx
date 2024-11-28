b3697657d87648596a867dfebcbce4bc5102d664
mehedihasan-ofc <mehedi.ofcc@gmail.com>
2024-11-28 23:37:45 +0600

    Update responsive mhhm 1 11 1 1

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -2,20 +2,32 @@ import { Outlet, useLocation } from "react-router-dom";
 import Header from "../pages/Shared/Header/Header";
 import Footer from "../pages/Shared/Footer/Footer";
 import { useEffect } from "react";
+import ScrollToTop from "react-scroll-to-top";
 
 const MainLayout = () => {
-    
+
     const { pathname } = useLocation();
-    
+
     useEffect(() => {
         window.scrollTo(0, 0);
     }, [pathname]);
-    
+
     return (
         <>
             <Header />
             <Outlet />
             <Footer />
+
+            <ScrollToTop
+                color="#3BB77E"
+                smooth
+                style={{
+                    borderRadius: "50%",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center"
+                }}
+            />
         </>
     );
 };
