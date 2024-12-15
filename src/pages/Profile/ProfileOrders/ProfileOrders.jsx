0599663d2a72be8455ca37498e4794394d67d954
mehedihasan-ofc <mehedi.ofcc@gmail.com>
2024-12-15 14:59:54 +0600

    some improves

--- src/pages/Profile/ProfileOrders/ProfileOrders.jsx
+++ src/pages/Profile/ProfileOrders/ProfileOrders.jsx
@@ -5,10 +5,36 @@ import { GiCheckMark } from "react-icons/gi";
 import { LuCopy } from "react-icons/lu";
 import MySpinner from "../../../components/Shared/MySpinner/MySpinner";
 import useOrders from "../../../hooks/useOrders";
-import { Button } from "@material-tailwind/react";
+import { Button, Tab, Tabs, TabsHeader } from "@material-tailwind/react";
+
+const TABS = [
+    {
+        label: "All",
+        value: "all",
+    },
+    {
+        label: "Pending",
+        value: "pending",
+    },
+    {
+        label: "Confirmed",
+        value: "confirmed",
+    },
+    {
+        label: "Packaging",
+        value: "packaging",
+    },
+    {
+        label: "Delivered",
+        value: "delivered",
+    }
+];
 
 const ProfileOrders = () => {
-    const [orders, isLoading] = useOrders();
+
+    const [activeTab, setActiveTab] = useState("all");
+
+    const [orders, isLoading] = useOrders(activeTab);
     const [copiedOrderId, setCopiedOrderId] = useState(false);
     const navigate = useNavigate();
 
@@ -48,15 +74,38 @@ const ProfileOrders = () => {
             <h2 className="text-2xl font-semibold text-gray-800 text-center mb-4">
                 My Orders
             </h2>
-            
+
             {orders.length === 0 ? (
                 <p className="text-gray-600 text-center">
                     You have no orders yet.
                 </p>
             ) : (
                 <div className="space-y-4">
-                    {orders.map((order) => (
 
+                    <Tabs value={activeTab}>
+                        <TabsHeader
+                            // className="rounded-none bg-secondary"
+                            className="rounded-none bg-secondary"
+                            indicatorProps={{
+                                className:
+                                    "shadow rounded-none",
+                            }}
+                        >
+                            {TABS.map(({ label, value }) => (
+                                <Tab
+                                    key={value}
+                                    value={value}
+                                    onClick={() => setActiveTab(value)}
+                                    // className="text-sm"
+                                    className={`text-xs md:text-base font-medium ${activeTab === value && "text-primary"}`}
+                                >
+                                    {label}
+                                </Tab>
+                            ))}
+                        </TabsHeader>
+                    </Tabs>
+
+                    {orders.map((order) => (
                         <div
                             key={order._id}
                             className="flex items-center justify-between border p-4 rounded shadow"
