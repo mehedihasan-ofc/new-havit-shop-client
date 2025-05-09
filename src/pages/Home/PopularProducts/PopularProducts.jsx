55e1308763b3c3d3dd4040e95b02f0b47716045d
mehedihasan-ofc <mehedi.ofcc@gmail.com>
2025-05-09 16:27:54 +0600

    Update promise secton xxx

--- src/pages/Home/PopularProducts/PopularProducts.jsx
+++ src/pages/Home/PopularProducts/PopularProducts.jsx
@@ -1,5 +1,4 @@
 import { Button } from "@material-tailwind/react";
-import { FaArrowRightLong } from "react-icons/fa6";
 import ProductCard from "../../../components/Card/ProductCard/ProductCard";
 import useProducts from "../../../hooks/useProducts";
 import { useNavigate } from "react-router-dom";
@@ -9,24 +8,24 @@ const PopularProducts = () => {
     const [products] = useProducts();
     const navigate = useNavigate();
 
-     // Early return if no categories exist
-     if (!products || products.length === 0) return null;
+    // Early return if no categories exist
+    if (!products || products.length === 0) return null;
 
     return (
         <div className="my-container">
 
-            <div className='flex justify-between items-center'>
-                <h4 className="text-base md:text-xl font-serif font-bold">Popular Products</h4>
-                <Button onClick={() => navigate("/products/all")} variant="text" className="flex items-center gap-2 font-serif px-4 py-2 rounded">
-                    See All
-                    <FaArrowRightLong />
-                </Button>
-            </div>
+            <h4 className="text-base md:text-xl font-serif font-bold">Popular Products</h4>
 
-            <div className="mt-5 grid grid-cols-2 md:grid-cols-4 gap-5">
-                {products.map(product => (
+            <div className="my-5 grid grid-cols-2 md:grid-cols-4 gap-5">
+                {products.slice(0, 20).map(product => (
                     <ProductCard key={product._id} product={product} />
                 ))}
+            </div>
+
+            <div className='text-center'>
+                <Button onClick={() => navigate("/products/all")} className="font-serif px-8 py-2 bg-primary rounded">
+                    See All
+                </Button>
             </div>
 
         </div>
--- src/pages/Home/SpecialOffers/SpecialOffers.jsx
+++ src/pages/Home/SpecialOffers/SpecialOffers.jsx
@@ -1,5 +1,4 @@
 import { Button } from "@material-tailwind/react";
-import { FaArrowRightLong } from "react-icons/fa6";
 import { useNavigate } from "react-router-dom";
 import useDiscountedProducts from "../../../hooks/useDiscountedProducts";
 import ProductCard from "../../../components/Card/ProductCard/ProductCard";
@@ -9,24 +8,22 @@ const SpecialOffers = () => {
     const [discountedProducts] = useDiscountedProducts();
     const navigate = useNavigate();
 
-     // Early return if no categories exist
-     if (!discountedProducts || discountedProducts.length === 0) return null;
+    // Early return if no categories exist
+    if (!discountedProducts || discountedProducts.length === 0) return null;
 
     return (
         <div className="my-container">
 
-            <div className='flex justify-between items-center'>
-                <h4 className="text-base md:text-xl font-serif font-bold">Special Offers</h4>
-                <Button onClick={() => navigate("/discounted-products")} variant="text" className="flex items-center gap-2 font-serif px-4 py-2 rounded">
-                    See All
-                    <FaArrowRightLong />
-                </Button>
-            </div>
+            <h4 className="text-base md:text-xl font-serif font-bold">Special Offers</h4>
 
-            <div className="mt-5 grid grid-cols-2 md:grid-cols-4 gap-5">
-                {discountedProducts.map(product => (
+            <div className="my-5 grid grid-cols-2 md:grid-cols-4 gap-5">
+                {discountedProducts.slice(0, 20).map(product => (
                     <ProductCard key={product._id} product={product} />
                 ))}
+            </div>
+
+            <div className='text-center'>
+                <Button className="font-serif px-8 py-2 bg-primary rounded" onClick={() => navigate("/discounted-products")}>See All</Button>
             </div>
 
         </div>
